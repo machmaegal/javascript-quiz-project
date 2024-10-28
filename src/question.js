ee5552989121fdb8197f3dd1bd6088f2f24ff0d3
machmaegal <aka.pleat371@passinbox.com>
2024-10-28 17:00:43 +0100

    day1/day2

--- src/question.js
+++ src/question.js
@@ -1,7 +1,16 @@
 class Question {
     // YOUR CODE HERE:
     //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        if (difficulty < 1 || difficulty > 3) this.difficulty = 1;
 
-    // 2. shuffleChoices()
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices() {
+        this.choices.sort(() => Math.random() - 0.5);
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,47 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
     // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
+    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        this.questions.sort(() => Math.random() - 0.5);
+    }
 
     // 5. checkAnswer(answer)
-
+    checkAnswer(answer) {
+        this.correctAnswers++;
+        return false;
+    }
     // 6. hasEnded()
+    hasEnded() {
+        if (this.questions.length === this.currentQuestionIndex) return true;
+        return false;
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty < 1 || difficulty > 3 || isNaN(difficulty)) return this.questions;
+        this.questions = this.questions.filter(question => question.difficulty === difficulty);
+    }
+
+    averageDifficulty() {
+        return this.questions.reduce((sum, question) => sum + question.difficulty, 0) / this.questions.length;
+    }
+
+
 }
