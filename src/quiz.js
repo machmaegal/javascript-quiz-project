8170056b5abec035971cce428b58753921b9baa1
Pandau <66787998+MrFootwork@users.noreply.github.com>
2024-10-29 17:13:18 +0100

    Pandau's solution (#1)
    * refactor html string
    
    * refactor quiz.js
    
    * make quiz fully functional
    
    * day 3 complete
    
    * day 4 complete

--- src/quiz.js
+++ src/quiz.js
@@ -1,48 +1,68 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
+	// YOUR CODE HERE:
+	//
+	// 1. constructor (questions, timeLimit, timeRemaining)
+	constructor(questions, timeLimit, timeRemaining) {
+		this.questions = questions;
+		this.timeLimit = timeLimit;
+		this.timeRemaining = timeRemaining;
+		this.correctAnswers = 0;
+		this.currentQuestionIndex = 0;
+	}
 
-    // 2. getQuestion()
-    getQuestion() {
-        return this.questions[this.currentQuestionIndex];
-    }
+	// 2. getQuestion()
+	getQuestion() {
+		return this.questions[this.currentQuestionIndex];
+	}
 
-    // 3. moveToNextQuestion()
-    moveToNextQuestion() {
-        this.currentQuestionIndex++;
-    }
-    // 4. shuffleQuestions()
-    shuffleQuestions() {
-        this.questions.sort(() => Math.random() - 0.5);
-    }
+	// 3. moveToNextQuestion()
+	moveToNextQuestion() {
+		this.currentQuestionIndex++;
+	}
 
-    // 5. checkAnswer(answer)
-    checkAnswer(answer) {
-        this.correctAnswers++;
-        return false;
-    }
-    // 6. hasEnded()
-    hasEnded() {
-        if (this.questions.length === this.currentQuestionIndex) return true;
-        return false;
-    }
+	// 4. shuffleQuestions()
+	shuffleQuestions() {
+		this.questions.sort(() => Math.random() - 0.5);
+	}
 
-    filterQuestionsByDifficulty(difficulty) {
-        if (difficulty < 1 || difficulty > 3 || isNaN(difficulty)) return this.questions;
-        this.questions = this.questions.filter(question => question.difficulty === difficulty);
-    }
+	// 5. checkAnswer(answer)
+	checkAnswer(answer) {
+		const answerIsCorrect =
+			this.questions[this.currentQuestionIndex].answer === answer;
 
-    averageDifficulty() {
-        return this.questions.reduce((sum, question) => sum + question.difficulty, 0) / this.questions.length;
-    }
+		if (answerIsCorrect) this.correctAnswers++;
+	}
 
+	// 6. hasEnded()
+	hasEnded() {
+		if (this.questions.length === this.currentQuestionIndex) return true;
+		return false;
+	}
 
+	filterQuestionsByDifficulty(difficulty) {
+		const difficultyIsInvalid =
+			difficulty < 1 || difficulty > 3 || isNaN(difficulty);
+
+		if (difficultyIsInvalid) return this.questions;
+
+		this.questions = this.questions.filter(
+			question => question.difficulty === difficulty
+		);
+	}
+
+	averageDifficulty() {
+		return (
+			this.questions.reduce((sum, { difficulty }) => sum + difficulty, 0) /
+			this.questions.length
+		);
+	}
+
+	getFormattedRemainingTime() {
+		const minutes = Math.floor(this.timeRemaining / 60)
+			.toString()
+			.padStart(2, '0');
+		const seconds = (this.timeRemaining % 60).toString().padStart(2, '0');
+
+		return `${minutes}:${seconds}`;
+	}
 }
